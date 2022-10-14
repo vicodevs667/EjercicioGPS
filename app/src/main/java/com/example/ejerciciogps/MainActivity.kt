a91c090d3771190caca76cfca35fed892a02b214
Victor Sanjines <victorsanjines667@gmail.com>
2022-10-14 17:35:01 -0400

    cambios configuracion gps

--- app/src/main/java/com/example/ejerciciogps/MainActivity.kt
+++ app/src/main/java/com/example/ejerciciogps/MainActivity.kt
@@ -9,9 +9,34 @@ import android.os.Bundle
 import android.provider.Settings
 import androidx.appcompat.app.AlertDialog
 import com.example.ejerciciogps.databinding.ActivityMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
 
 class MainActivity : AppCompatActivity() {
+    //Definir valores globales de tu clase que no necesitan
+    //inicialización y representan lo mismo como constantes
+    //para todas las instancias de tu clase
+    //Definir como constantes globales de esta clase
+    companion object {
+        val PERMISSION_GRANTED = arrayOf(
+            android.Manifest.permission.ACCESS_COARSE_LOCATION,
+            android.Manifest.permission.ACCESS_FINE_LOCATION,
+        )
+    }
+
+
     private lateinit var binding : ActivityMainBinding
+
+    //Nos indica si el GPS esta habilitado o no
+    private var enabledGPS = false
+    //Variable fundamental para el tratamiento de la información
+    //que refiere a las coordenadas que nos va a devolver el servicio
+    //del GPS.
+    //Este objeto conjunciona todos los datos referentes a localización.
+    //Fusiona toda la data referida a GPS. y la usaremos a partir
+    // de una librería de los servicios de Google.
+    private lateinit var fusedLocation: FusedLocationProviderClient
+    //Vamos a necesitar un código de validación de permisos otorgados
+    private val PERMISSION_ID = 42
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,16 +56,26 @@ class MainActivity : AppCompatActivity() {
                 dialog, wich -> goToGPSSettings()
             })
             .setNegativeButton(R.string.dialog_button_denied) {
-                dialog, wich -> cancelGPS()
+                dialog, wich -> enabledGPS = false
             }
-    }
-
-    private fun cancelGPS() {
-        TODO("Not yet implemented")
+            .setCancelable(true)
+            .show()
     }
 
     private fun goToGPSSettings() {
-        TODO("Not yet implemented")
+        enabledGPS = true
+        val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+        startActivity(intent)
+    }
+
+    //Método para validar si tenemos o no habilitado el servicio GPS
+    private fun hasEnabledGPS(): Boolean {
+        //Gestor u orquestador de lo referente a tratamiento de
+        //el GPS.......
+        val locationManager: LocationManager =
+            getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
     }
 }
 
