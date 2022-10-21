9b82f6e8fc67a94eb8680984bc6602eb80211c25
Victor Sanjines <victorsanjines667@gmail.com>
2022-10-21 15:27:51 -0400

    configuracion de variables para permisos de GPS

--- app/src/main/java/com/example/ejerciciogps/MainActivity.kt
+++ app/src/main/java/com/example/ejerciciogps/MainActivity.kt
@@ -21,10 +21,73 @@ import com.google.android.gms.location.*
 import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
+    //OJITO: esta forma no es necesariamente
+    //obligatoria de usar para este dato, solo es referencial
+    //companion object se usa para definir
+    //constantes que serán globales en tu clase
+    //que sus valores son accedidos por cualquier instancia de esta
+    companion object {
+        val PERMISSION_GRANTED = arrayOf(
+            android.Manifest.permission.ACCESS_COARSE_LOCATION,
+            android.Manifest.permission.ACCESS_FINE_LOCATION,
+            android.Manifest.permission.ACCESS_NETWORK_STATE
+        )
+    }
+
+    private lateinit var binding: ActivityMainBinding
+
+    //Nosotros usaremos una herramienta de los servicios de Google para localización
+    private lateinit var fusedLocation: FusedLocationProviderClient
+    private val PERMISSION_ID = 42
+    private var isGpsEnabled = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.fabGps.setOnClickListener {
+            enableGPSService()
+        }
+
+    }
+
+    /**
+     * Situación: Configurar la habilitación
+     *              de GPS en el celular
+     */
+    private fun enableGPSService() {
+        if (!hasGPSEnabled()) {
+            AlertDialog.Builder(this)
+                .setTitle(R.string.dialog_text_title)
+                .setMessage(R.string.dialog_text_description)
+                .setPositiveButton(R.string.dialog_button_accept,
+                    DialogInterface.OnClickListener{
+                            dialog, wich -> goToEnableGPS()
+                    })
+                .setNegativeButton(R.string.dialog_button_deny) {
+                        dialog, wich -> isGpsEnabled = false
+                }
+                .setCancelable(true)
+                .show()
+        } else
+            Toast.makeText(this, "El GPS ya esta activado",
+                Toast.LENGTH_SHORT).show()
+    }
+
+    private fun goToEnableGPS() {
+        val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+        startActivity(intent)
+    }
+
+    private fun hasGPSEnabled(): Boolean {
+        //Manager: Orquestador o director de la orquesta
+        //      es el que lleva la batuta
+        //      organiza y gestiona lo referido al manejo
+        //      de cierto servicio o recurso
+        val locationManager: LocationManager =
+            getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
     }
 
 }
