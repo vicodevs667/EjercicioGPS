8fa02c5e0fa7db7dcdf16da2f2e4b93edcf58865
Victor Sanjines <victorsanjines667@gmail.com>
2022-10-25 08:06:16 -0400

    obtencion de coordenadas

--- app/src/main/java/com/example/ejerciciogps/MainActivity.kt
+++ app/src/main/java/com/example/ejerciciogps/MainActivity.kt
@@ -13,9 +13,11 @@ import android.os.Looper
 import android.provider.Settings
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.constraintlayout.motion.widget.Debug.getLocation
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.location.LocationCompat
+import androidx.core.location.LocationManagerCompat.getCurrentLocation
 import com.example.ejerciciogps.databinding.ActivityMainBinding
 import com.google.android.gms.location.*
 import java.util.jar.Manifest
@@ -47,6 +49,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.fabGps.setOnClickListener {
             enableGPSService()
+        }
+        binding.fabCoordenadas.setOnClickListener {
+
         }
 
     }
@@ -88,6 +93,85 @@ class MainActivity : AppCompatActivity() {
             getSystemService(Context.LOCATION_SERVICE) as LocationManager
         return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
                 || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    /**
+     * Situación: Configuración y solicitud de permisos
+     * en la APP para poder usar GPS
+     */
+    // checkSelfPermission: evalua el valor que tiene
+    // en tu app cierto permiso, no verifica si tienes o no permiso
+    // solo ve que valor numérico tiene asignado ese permiso en tu app
+    // PERMISSION_GRANTED: es un valor numérico general en Android
+    // que representa el valor que significa un permiso otorgado
+    // Revisan si Android tiene como permiso otorgado, los permisos
+    // que estan revisando en este método
+    private fun allPermissionsGranted(): Boolean =
+        PERMISSION_GRANTED.all {
+            ActivityCompat.checkSelfPermission(baseContext, it) ==
+                    PackageManager.PERMISSION_GRANTED
+        }
+
+    private fun checkPermission(): Boolean {
+        return ActivityCompat.checkSelfPermission(this,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this,
+        android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestPermissionUser() {
+        //Lanzar la ventana al usuario
+        //para solicitarle que habilite permisos o los deniegue
+        ActivityCompat.requestPermissions(
+            this,
+            PERMISSION_GRANTED,
+            PERMISSION_ID
+        )
+    }
+
+    /**
+     * Situación: obtención de coordenadas
+     * configuración de objeto que trabaja con el sensor
+     * y obtiene Localizaciones llamado FusedLocation
+     */
+
+    @SuppressLint("MissingPermission")
+    private fun manageLocation() {
+        if (hasGPSEnabled()) {
+            if (allPermissionsGranted()) {
+                fusedLocation = LocationServices.getFusedLocationProviderClient(this)
+                fusedLocation.lastLocation.addOnSuccessListener {
+                    location -> getCoordinates()
+                }
+            }
+        } else
+            goToEnableGPS()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getCoordinates() {
+        val locationRequest = LocationRequest.create().apply {
+            priority = Priority.PRIORITY_HIGH_ACCURACY
+            interval = 0
+            fastestInterval = 0
+            numUpdates = 1
+        }
+        fusedLocation.requestLocationUpdates(
+            locationRequest,
+            myLocationCallback,
+            Looper.myLooper()
+        )
+    }
+
+    private val myLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            super.onLocationResult(locationResult)
+            if (locationResult != null) {
+                var myLastLocation = locationResult.lastLocation
+                binding.txtLatitud.text = myLastLocation.latitude.toString()
+                binding.txtLongitud.text = myLastLocation.longitude.toString()
+            }
+        }
     }
 
 }
