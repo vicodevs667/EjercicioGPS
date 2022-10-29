29ac39bd4a8417708725e2fbd602c5d1b4335651
Victor Sanjines <victorsanjines667@gmail.com>
2022-10-29 09:36:15 -0400

    configuracion de camara personalizada en mapas

---
+++ app/src/main/java/com/example/ejerciciogps/Coordenadas.kt
@@ -0,0 +1,17 @@
+package com.example.ejerciciogps
+
+import com.google.android.gms.maps.model.LatLng
+
+object Coordenadas {
+    val univalle = LatLng(-16.50332715970661, -68.11993158748582)
+    val stadium = LatLng(-16.49933544447848, -68.12267410802554)
+    val avaroa = LatLng(-16.51052369694955, -68.1265779779598)
+    val jardinJapones = LatLng(-16.54094219934167, -68.09206070650323)
+    val valleLuna = LatLng(-16.567384855433435, -68.09385445864866)
+}
+
+
+
+
+
+
--- app/src/main/java/com/example/ejerciciogps/MapsActivity.kt
+++ app/src/main/java/com/example/ejerciciogps/MapsActivity.kt
@@ -2,6 +2,9 @@ package com.example.ejerciciogps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import com.example.ejerciciogps.Coordenadas.avaroa
+import com.example.ejerciciogps.Coordenadas.univalle
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -10,9 +13,13 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.ejerciciogps.databinding.ActivityMapsBinding
+import com.google.android.gms.maps.model.CameraPosition
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    //Variable global que representa su mapa de Google
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
 
@@ -23,8 +30,13 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         setContentView(binding.root)
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        //Concepto clave y avanzado de Android.
+        //Fragmentos: seccionar en piezas el diseño de UI
+        //Fragmento que contiene el mapa de Google
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
+        //El mapa de Google se obtine de manera asíncrona
+        //busca cargar el mapa sin congelar tu pantalla
         mapFragment.getMapAsync(this)
     }
 
@@ -37,12 +49,89 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+    //Lo que contiene se ejecuta cuando el mapa
+    //esta listo y cargado en tu pantalla
+    //como parámetro recibes un objeto que contiene
+    //el mapa listo para trabajar........
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        //una posición.
+        //se maneja un objeto que conjunciona latitud y longitud
+        //ese objeto se llama LatLng
+        val egipto = LatLng(29.977704826716288, 31.13258127087077)
+
+        //Definir algunas configuraciones de su mapa
+        //Los mapas manejan un zoom y este va de 0 a 21
+        //donde jamas vas a poder usar el valor 0 o 1
+        //el zoom minimo es de 2.....
+        //el zoom máximo es de 20
+        /*
+            zoom de 20: se usa para edificios, construcciones
+                        casas, parques, detalles muy finos.
+            a partir de 15: se suele usar para calles
+            a partir de 10: para ciudades
+            a partir de 5: para paises y continente
+            a partir de 2: continente y su masa continental
+         */
+        mMap.apply {
+            //configurar el zoom mínimo que pueden usar en su mapa
+            setMinZoomPreference(15f)
+            //El máximo zoom que puede hacer el usuario
+            setMaxZoomPreference(20f)
+        }
+
+        //Uso de marcadores o las famosas tachuelas rojas
+        mMap.addMarker(MarkerOptions()
+            .title("Mi viaje de ensueño")
+            .snippet("${egipto.latitude}, ${egipto.longitude}")
+            .position(egipto)
+        )
+
+        //Realizando el movimiento de la cámara a la posición
+        //deseada por usted
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(egipto))
+        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(egipto, 18f))
+
+        //Configurar una cámara personalizada
+        val cameraUnivalle = CameraPosition.Builder()
+            .target(univalle)//ubicación donde va a centrarse la cámara
+            .zoom(18f)
+            .tilt(35f)//ángulo de inclinación de la cámara
+            .bearing(178f)// ángulo para cambiar orientación de vista del mapa 0-360
+            .build()
+        //mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraUnivalle))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(univalle, 18f))
+
+        //Movimiento de cámara utilizando procesos en background
+        //utilizando Corrutinas
+        lifecycleScope.launch {
+            delay(5000)
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(avaroa,18f))
+        }
+
+
+
+
+
+        //Vamos a configurar el evento más simple
+        //y útil de los mapas de google
+        // el click en cualquier lugar del mapa
+        mMap.setOnMapClickListener {
+            mMap.addMarker(MarkerOptions()
+                .title("Posicion Random")
+                .snippet("${it.latitude}, ${it.longitude}")
+                .draggable(true)
+                .position(it)
+            )
+        }
     }
 }
+
+
+
+
+
+
+
+
